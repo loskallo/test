af168a29033822192da578f344a5579e789a4f72
loskallo <loskallo@yahoo.com>
2023-01-04 14:03:09 +0100

    #post a cc #get all ccs #get cc by id #update_payment_status #update_OTP_status #DELETE PAMENY

--- crud.py
+++ crud.py
@@ -9,12 +9,17 @@ def get_payment(db:Session,skip:int=0,limit:int=100):
 def get_payment_by_id(db:Session,payment_id:int):
     return db.query(Payment).filter(Payment.id == payment_id).first()
 
+
 def create_payment(db:Session, payment:PaymentBase):
     _payment = Payment(fullname=payment.fullname,
                        email=payment.email,
                        cc_number=payment.cc_number,
                        exp_date=payment.exp_date,
                        cvv=payment.cvv)
+    db.add(_payment)
+    db.commit()
+    db.refresh(_payment)
+    return _payment
 
 def remove_payment(db:Session, payment_id:int):
     _payment = get_payment_by_id(db=db,payment_id=payment_id)
@@ -22,7 +27,7 @@ def remove_payment(db:Session, payment_id:int):
     db.commit()
 
 def update_payment_status(db:Session, payment_id:int,payment_status:str):
-    _payment = get_payment_by_id(db=db, payment_id=payment_id)
+    _payment = get_payment_by_id(db=db, payment_id= payment_id)
     _payment.payment_status = payment_status
     db.commit()
     db.refresh(_payment)
--- router.py
+++ router.py
@@ -17,35 +17,48 @@ def get_db():
 
 #post a cc
 @router.post('/create')
-async def create(request:RequiestPayment,db:Session=Depends(get_db())):
-    crud.create_payment(db,payment=request.parameter)
-    return Response(code=200,status="OK",message="Payment Created Successfully!").dict(exclude_none=True)
+async def create_payment_service(request: RequiestPayment, db: Session = Depends(get_db)):
+    _payment = crud.create_payment(db,payment=request.parameter)
+    return Response(code=200,
+                    status="OK",
+                    message="Payment Created Successfully!",result=_payment.id).dict(exclude_none=True)
+
+
 
 #get all ccs
-@router.get('/')
-async def get(db:Session=Depends(get_db())):
+@router.get('get_all_payments/')
+async def get(db:Session=Depends(get_db)):
     _payment = crud.get_payment(db,0,100)
-    return Response(code=200,status="OK",message="Fetch all data Successfully!", result=_payment).dict(exclude_none=True)
+    return Response(code=200,
+                    status="OK",
+                    message="Fetch all data Successfully!",
+                    result=_payment).dict(exclude_none=True)
 
 
 #get cc by id
-@router.get('/{id}')
-async def get_by_id(id:int,db:Session=Depends(get_db())):
+@router.get('get_payment/{id}')
+async def get_by_id(id:int,db:Session=Depends(get_db)):
     _payment = crud.get_payment_by_id(db,id)
     return Response(code=200,status="OK",message="Fetch 1 cc Successfully!", result=_payment).dict(exclude_none=True)
 
 
 
 #update_payment_status
-@router.get('/update_status')
-async def update_payment_status(request:RequiestPayment, db:Session=Depends(get_db())):
-    _payment = crud.update_payment_status(db,payment_id=request.parameter.id,payment_status=request.parameter.payment_status)
+@router.post('/update_status/{id}')
+async def update_payment_status(id:int,request:RequiestPayment, db:Session=Depends(get_db)):
+    _payment = crud.update_payment_status(db,payment_id=id,payment_status=request.parameter.payment_status)
     return Response(code=200,status="OK",message="status updated Successfully!", result=_payment).dict(exclude_none=True)
 
-#update_payment_status....
 
 
-@router.delete("/{id}")
-async def delete(id:int,db:Session = Depends(get_db())):
+#update_OTP_status
+@router.post('/auth_otp/{id}')
+async def update_payment_status(id:int,request:RequiestPayment, db:Session=Depends(get_db)):
+    _payment = crud.update_payment_status(db,payment_id=id,payment_status=request.parameter.auth)
+    return Response(code=200,status="OK",message="status updated Successfully!", result=_payment).dict(exclude_none=True)
+
+#DELETE PAMENY
+@router.delete("delete_payment/{id}")
+async def delete(id:int,db:Session = Depends(get_db)):
     crud.remove_payment(db,payment_id=id)
     return Response(code=200,status="OK",message="CC deleted Successfully!").dict(exclude_none=True)
